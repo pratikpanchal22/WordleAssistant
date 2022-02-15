d96f8d2b90bddac8a32ac25c28f7e88b313623f1
Pratik Panchal <pp423@cornell.edu>
2022-02-14 21:14:02 -0700

    Moved string constants

--- src/main/java/com/pratikpanchal/wordle/hintprovider/HintProvider.java
+++ src/main/java/com/pratikpanchal/wordle/hintprovider/HintProvider.java
@@ -3,6 +3,34 @@ package com.pratikpanchal.wordle.hintprovider;
 import java.util.Arrays;
 
 public class HintProvider {
+
+    public static final String HINT_GGGGG_ALL_MATCH = "GGGGG";
+    public static final String HINT_BBBBB_NO_MATCH = "BBBBB";
+    public static final String HINT_BGBYB = "BGBYB";
+    public static final String HINT_YGBBB = "YGBBB";
+    public static final String WORD_WATER = "water";
+    public static final String WORD_SALTY = "salty";
+    public static final String WORD_TASTY = "tasty";
+    public static final String HINT_BGYBB = "BGYBB";
+    public static final String HINT_GBBGY = "GBBGY";
+    public static final String WORD_TTXTS = "ttxts";
+    public static final String HINT_GYYGG = "GYYGG";
+    public static final String HINT_GYGGY = "GYGGY";
+    public static final String HINT_YYGGY = "YYGGY";
+    public static final String HINT_GBGBB = "GBGBB";
+    public static final String HINT_BGBGB = "BGBGB";
+    public static final String HINT_BBBBG = "BBBBG";
+    public static final String HINT_BBBBB = "BBBBB";
+    public static final String HINT_GGGGG = "GGGGG";
+    public static final String WORD_AABBC = "aabbc";
+    public static final String WORD_ACABB = "acabb";
+    public static final String WORD_CAABB = "caabb";
+    public static final String WORD_AAAAA = "aaaaa";
+    public static final String WORD_BBBBB = "bbbbb";
+    public static final String WORD_CCCCC = "ccccc";
+    public static final String WORD_XXXXX = "xxxxx";
+    public static final String WORD_ABABC = "ababc";
+
     private String secretWord;
 
     public HintProvider(String secretWord) {
--- src/test/java/com/pratikpanchal/wordle/hintprovider/HintProviderTest.java
+++ src/test/java/com/pratikpanchal/wordle/hintprovider/HintProviderTest.java
@@ -3,53 +3,27 @@ package com.pratikpanchal.wordle.hintprovider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Test: HintProvider behavior")
 public class HintProviderTest {
 
-    public static final String HINT_BGBYB = "BGBYB";
-    public static final String HINT_YGBBB = "YGBBB";
-    public static final String WORD_WATER = "water";
-    public static final String WORD_SALTY = "salty";
-    public static final String WORD_TASTY = "tasty";
-    public static final String HINT_BGYBB = "BGYBB";
-    public static final String HINT_GBBGY = "GBBGY";
-    public static final String WORD_TTXTS = "ttxts";
-    public static final String HINT_GYYGG = "GYYGG";
-    public static final String HINT_GYGGY = "GYGGY";
-    public static final String HINT_YYGGY = "YYGGY";
-    public static final String HINT_GBGBB = "GBGBB";
-    public static final String HINT_BGBGB = "BGBGB";
-    public static final String HINT_BBBBG = "BBBBG";
-    public static final String HINT_BBBBB = "BBBBB";
-    public static final String HINT_GGGGG = "GGGGG";
-    public static final String WORD_AABBC = "aabbc";
-    public static final String WORD_ACABB = "acabb";
-    public static final String WORD_CAABB = "caabb";
-    public static final String WORD_AAAAA = "aaaaa";
-    public static final String WORD_BBBBB = "bbbbb";
-    public static final String WORD_CCCCC = "ccccc";
-    public static final String WORD_XXXXX = "xxxxx";
-    public static final String WORD_ABABC = "ababc";
-
     @Test
     @DisplayName("Test: Secret word with no repeating characters")
     public void uniqueCharsTest(){
-        HintProvider hintProvider = new HintProvider(WORD_WATER);
+        HintProvider hintProvider = new HintProvider(HintProvider.WORD_WATER);
 
-        assertEquals(HINT_BGBYB, hintProvider.provideHint(WORD_SALTY));
-        assertEquals(HINT_YGBBB, hintProvider.provideHint(WORD_TASTY));
+        assertEquals(HintProvider.HINT_BGBYB, hintProvider.provideHint(HintProvider.WORD_SALTY));
+        assertEquals(HintProvider.HINT_YGBBB, hintProvider.provideHint(HintProvider.WORD_TASTY));
         //TODO: Add more tests uniqueCharsTest
     }
 
     @Test
     @DisplayName("Test: Secret word with 1 repeating character of frequency 2")
     public void repeatingChars1Test(){
-        HintProvider hintProvider = new HintProvider(WORD_TASTY);
+        HintProvider hintProvider = new HintProvider(HintProvider.WORD_TASTY);
 
-        assertEquals(HINT_BGYBB, hintProvider.provideHint(WORD_WATER));
-        assertEquals(HINT_GBBGY, hintProvider.provideHint(WORD_TTXTS));
+        assertEquals(HintProvider.HINT_BGYBB, hintProvider.provideHint(HintProvider.WORD_WATER));
+        assertEquals(HintProvider.HINT_GBBGY, hintProvider.provideHint(HintProvider.WORD_TTXTS));
         //TODO: Add more tests to repeatingChars1Test
     }
 
@@ -58,35 +32,36 @@ public class HintProviderTest {
     public void repeatingChars2Test(){
         HintProvider hintProvider = new HintProvider("ababc");
 
-        assertEquals(HINT_GYYGG, hintProvider.provideHint(WORD_AABBC));
-        assertEquals(HINT_GYGGY, hintProvider.provideHint(WORD_ACABB));
-        assertEquals(HINT_YYGGY, hintProvider.provideHint(WORD_CAABB));
-        assertEquals(HINT_GBGBB, hintProvider.provideHint(WORD_AAAAA));
-        assertEquals(HINT_BGBGB, hintProvider.provideHint(WORD_BBBBB));
-        assertEquals(HINT_BBBBG, hintProvider.provideHint(WORD_CCCCC));
-        assertEquals(HINT_BBBBB, hintProvider.provideHint(WORD_XXXXX));
-        assertEquals(HINT_GGGGG, hintProvider.provideHint(WORD_ABABC));
+        assertEquals(HintProvider.HINT_GYYGG, hintProvider.provideHint(HintProvider.WORD_AABBC));
+        assertEquals(HintProvider.HINT_GYGGY, hintProvider.provideHint(HintProvider.WORD_ACABB));
+        assertEquals(HintProvider.HINT_YYGGY, hintProvider.provideHint(HintProvider.WORD_CAABB));
+        assertEquals(HintProvider.HINT_GBGBB, hintProvider.provideHint(HintProvider.WORD_AAAAA));
+        assertEquals(HintProvider.HINT_BGBGB, hintProvider.provideHint(HintProvider.WORD_BBBBB));
+        assertEquals(HintProvider.HINT_BBBBG, hintProvider.provideHint(HintProvider.WORD_CCCCC));
+        assertEquals(HintProvider.HINT_BBBBB, hintProvider.provideHint(HintProvider.WORD_XXXXX));
+        assertEquals(HintProvider.HINT_GGGGG, hintProvider.provideHint(HintProvider.WORD_ABABC));
         //TODO: Add more tests to repeatingChars1Test
     }
 
     @Test
     @DisplayName("Test: Hint provider initialized with secret word in all UPPER CASE")
     public void secretWordCaseMisMatchTest(){
-        HintProvider hintProvider = new HintProvider(WORD_TASTY.toUpperCase());
+        HintProvider hintProvider = new HintProvider(HintProvider.WORD_TASTY.toUpperCase());
 
-        assertEquals(HINT_BGYBB, hintProvider.provideHint(WORD_WATER));
-        assertEquals(HINT_GBBGY, hintProvider.provideHint(WORD_TTXTS));
+        assertEquals(HintProvider.HINT_BGYBB, hintProvider.provideHint(HintProvider.WORD_WATER));
+        assertEquals(HintProvider.HINT_GBBGY, hintProvider.provideHint(HintProvider.WORD_TTXTS));
         //TODO: Add more tests to secretWordCaseMisMatchTest
     }
 
     @Test
     @DisplayName("Test: Hint provider provideHint method invoked with words in all UPPER CASE")
     public void guessedWordCaseMisMatchTest(){
-        HintProvider hintProvider = new HintProvider(WORD_TASTY);
+        HintProvider hintProvider = new HintProvider(HintProvider.WORD_TASTY);
 
-        assertEquals(HINT_BGYBB, hintProvider.provideHint(WORD_WATER.toUpperCase()));
-        assertEquals(HINT_GBBGY, hintProvider.provideHint(WORD_TTXTS.toUpperCase()));
+        assertEquals(HintProvider.HINT_BGYBB, hintProvider.provideHint(HintProvider.WORD_WATER.toUpperCase()));
+        assertEquals(HintProvider.HINT_GBBGY, hintProvider.provideHint(HintProvider.WORD_TTXTS.toUpperCase()));
         //TODO: Add more tests to guessedWordCaseMisMatchTest
+
     }
 
     //TODO: Add more tests for HintProviderTest
